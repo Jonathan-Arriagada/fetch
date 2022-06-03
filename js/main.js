1faecaf99c62d6bf68ad6e813c700a07291a3f76
JonathanArriagada <jonathan.arriagada@live.com>
2022-06-02 21:31:17 -0300

    agregando fetch

---
+++ js/main.js
@@ -0,0 +1,118 @@
+const divProductos = document.getElementById("divProductos")
+const contenedorTabla = document.getElementById("tablaCarrito")
+const totalCarrito = document.getElementById("totalCarrito")
+const botonFinalizar = document.getElementById('botonFinalizar')
+let dataProductos
+
+document.addEventListener('DOMContentLoaded', () => { 
+    traerJson()
+    mostrarCarrito()
+    totalProductos()
+});
+
+const traerJson = async() => { 
+    let response = await fetch("js/productos.json")
+    let data = await response.json()
+    dataProductos = data
+    mostrarProducto(dataProductos)
+}   
+
+function mostrarProducto(array){
+    array.forEach(producto => {
+    divProductos.innerHTML += `
+    <div class="col mb-5">
+    <div class="card h-100">
+        <img class="card-img-top" src="${producto.img}"/>
+        <div class="card-body p-4">
+            <div class="text-center">
+                <h5 class="fw-bolder">${producto.nombre}</h5>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent"></div>
+             <div class="text-center">
+                <button onclick="agregar(${producto.id})" class="btn btn-outline-dark mt-auto">Agregar al carrito</button>
+             </div>
+                   
+        </div>
+    </div>
+    </div>
+ `
+
+ }); 
+}
+
+function mostrarCarrito(){
+    let carrito = capturarStorage()
+    contenedorTabla.innerHTML =""
+    carrito.forEach(producto => {
+        contenedorTabla.innerHTML += ` 
+        <tr>
+            <th scope="row">${producto.cantidad}</th>
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+            <td>${producto.precio * producto.cantidad}</td>
+            <td><button onclick="eliminarCarrito(${producto.id})">Eliminar</button></td>            
+        </tr>
+       `
+    })
+}
+
+function capturarStorage(){
+    return JSON.parse(localStorage.getItem("carrito")) || []
+
+}
+
+function guardarStorage(array){
+    localStorage.setItem("carrito", JSON.stringify(array))
+}
+
+function agregar(idParam){
+    let carrito = capturarStorage()
+    if(productoEnCarrito(idParam)){
+        incrementarCantidad(idParam)
+
+    }else{
+    let productoEncontrado = dataProductos.find(producto=>producto.id==idParam)
+    carrito.push({...productoEncontrado, cantidad:1 })
+    guardarStorage(carrito)
+    mostrarCarrito()
+    }
+    totalProductos()
+}
+
+function incrementarCantidad(id){
+    let carrito = capturarStorage()
+    const indice = carrito.findIndex(producto=>producto.id==id)
+    carrito[indice].cantidad++
+    guardarStorage(carrito)
+    mostrarCarrito()
+}
+function eliminarCarrito(id){
+    let carrito = capturarStorage()
+    let resultado = carrito.filter(producto=> producto.id !=id)
+    guardarStorage(resultado)
+    mostrarCarrito()
+    totalProductos()
+}
+
+function productoEnCarrito(id){
+    let carrito=capturarStorage()
+    return carrito.some(producto=>producto.id==id)
+}
+
+function totalProductos(){
+    let carrito=capturarStorage()
+    let total = carrito.reduce(
+        (acc, producto) => acc + producto.cantidad * producto.precio, 0
+    );
+    totalCarrito.innerHTML = total;
+}
+botonFinalizar.addEventListener('click',() => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Compra Exitosa',
+    })
+    localStorage.clear();
+    mostrarCarrito();
+    totalProductos();
+})
